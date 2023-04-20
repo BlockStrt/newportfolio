e2d72f45b145b013c842ac38f505773124b9356b
Blck Strt <blockstarter34@gmail.com>
2023-04-20 18:55:18 -0500

    update

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Image from 'next/image'
 import SectionTitle from './SectionTitle'
-import { p1 } from '@/public/assets'
+import {RxOpenInNewWindow} from 'react-icons/rx'
+
+import { p1, p2, p3, p4 } from '@/public/assets'
 
 const Projects = () => {
   return (
@@ -8,12 +11,41 @@ const Projects = () => {
         <SectionTitle title='Creations I`ve engineered' titleNo='03'/>
         {/* project one */}
         <div className='w-full flex flex-col items-center justify-center gap-28 mt-10'>
-            <div>
-                <div>
-              ``
+            <div className='flex flex-col xl:flex-row gap-6'>
+              <a 
+               className='w-full xl:w-1/2 h-auto relative group'
+               href="https://hammerandhope.org"
+               target='_blank'>
+               <div>
+              <Image
+               className='w-full h-full object-contain'
+               src={p1} alt='hammerImg'/>
                 </div>
+               </a>
+               <div className='w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between
+            items-end text-right xl:nl-16 z-10'>
+              <p className='font-titleFont text-textGreen text-sm tracking-wide'>Featured Project</p>
+              <h3 className='text-2xl font-bold'>Hammer&Hope</h3>
+              <p className='bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md'>
+              Magazine based on <span className='text-textGreen'>black politics & culture</span> 
+              </p>
+              <ul className='text-xs md:text-sm font-titleFont tracking-wide flex gap-2
+              md:gap-5 justify-between text-textDark'>
+                <li>NextJS</li>
+                <li>React</li>
+                <li>Sass</li>
+              </ul>
+              <div className='text-2xl flex gap-4'>
+                <a 
+                className='hover:text-textGreen duration-300'
+                href="https://hammerandhope.org"
+                target='_blank'>
+                  <RxOpenInNewWindow/>
+                </a>
+              </div>
+            </div>
             </div>  
-            <div className=''></div>
+           
         </div>
         {/* project two */}
         {/* project three */}
--- public/assets/index.tsx
+++ public/assets/index.tsx
@@ -1,6 +1,9 @@
 import logo from './images/logo.png'
 import pfp from './images/pfp.jpeg'
+import p1 from './images/hammershot.jpeg'
+import p2 from './images/jugshot.jpeg'
+import p3 from './images/halfshot.jpeg'
+import p4 from './images/mxdshot.jpeg'
 
 
-
-export { logo, pfp,};
+export { logo, pfp, p1, p2, p3, p4};
