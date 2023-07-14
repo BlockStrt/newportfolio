5ffb6c78f4d55deb034032859daf27fab1df6346
Blck Strt <blockstarter34@gmail.com>
2023-07-14 10:22:53 -0500

    update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
         items-center tracking-wide'>04. What`s Next?</p>
         <h2 className='font-titleFont text-5xl font-semibold'>Get in Touch</h2>
         <p className='max-w-[600px] text-center text-textDark'>
-            Currently seeking employment. As a motivated web developer, you are tirelessly seeking new opportunities to expand your skill set and make meaningful contributions to the ever-evolving digital landscape.
+            Currently seeking employment. As a motivated developer, you are tirelessly seeking new opportunities to expand your skill set and make meaningful contributions to the ever-evolving digital landscape.
         </p>
         <a href="mailto:blockstarter34@Gmail.com">
         <button className='w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm
